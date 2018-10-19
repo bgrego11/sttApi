cb054cccced068c60e1193066ef842f2f2bf73bb
bgrego11 <natureboy4271@gmail.com>
2018-10-19 19:46:00 -0400

    little h

--- sttApi/settings.py
+++ sttApi/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+
+
+from django.conf.locale.en import formats as en_formats 
+en_formats.DATETIME_FORMAT = "d-m-Y h:i:s"
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
