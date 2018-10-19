49cfbe9cd127e0b4171062058fb49ec1be593ea1
bgrego11 <natureboy4271@gmail.com>
2018-10-19 19:53:02 -0400

    admin

--- events/admin.py
+++ events/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["name", "logdate", "address"]
+    def time_seconds(self, obj):
+        return obj.datetimefield.strftime("%d %b %Y %h:%M:%S")
+    time_seconds.admin_order_field = 'datetimefield'
+    time_seconds.short_description = 'Precise Time' 
+   
+
+admin.site.register(Event, EventAdmin)
