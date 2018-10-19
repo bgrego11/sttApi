2781428f488d7bc8d4a3f513ea811028f2c6eb0a
bgrego11 <natureboy4271@gmail.com>
2018-10-19 19:27:01 -0400

    back

--- events/models.py
+++ events/models.py
@@ -9,8 +9,7 @@ class Event(models.Model):
     logdate = models.DateTimeField()
     address = models.TextField()
     created_date = models.DateTimeField(
-            default="America/New_York",)
-            #  input_formats=['%b %d, %Y']
+            default=timezone.now, input_formats=['%b %d, %Y'])
 
 
     def __str__(self):
